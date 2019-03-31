35cc59f8aff447ab7c2637e2e2c4ebd1ad708505
2019jrhee <2019jrhee@dwight.edu>
2019-03-31 12:30:12 -0400

    Flask_Blog_IA Authentification, first commit

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email
+from flaskblog.models import User
 
+# components for registering 
 class RegistrationForm(FlaskForm):
 	username = StringField('Username', 
 		validators= [DataRequired(), Length(min=5, max=20)])
@@ -13,12 +15,23 @@ class RegistrationForm(FlaskForm):
 
 	submit = SubmitField('Register')
 
+	#def validate_username(self, username): put ValidationError next to Email on the top
 
+		#user = User.query.filter_by(username=username.data).first()
+		#if user:
+			#raise ValidationError('This username already exists. Try a new one!')
 
+	#def validate_email(self, email):
+
+		#user = User.query.filter_by(email=email.data).first()
+		#if user:
+			#raise ValidationError('This email already exists. Try a new one!')
+
+# components for logging in 
 class LoginForm(FlaskForm):
 	username = StringField('Username', 
 		validators= [DataRequired(), Length(min=5, max=20)])
-	
+
 
 	password = PasswordField('Password', validators= [DataRequired()])
 
